83d32c6b930f211454fe27797fa6e7b8313d13a8
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-18 19:05:06 +0200

    completed queries with JOIN

--- ex_1.sql
+++ ex_1.sql
@@ -1,6 +1,6 @@
 /* GROUP BY QUERIES */
 --  1. Contare quanti iscritti ci sono stati ogni anno
-SELECT COUNT(*)
+SELECT COUNT(*) as `number of students`
 FROM `students`
 GROUP BY YEAR(`enrolment_date`);
 
@@ -24,7 +24,7 @@ GROUP BY `department_id`;
 --  1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`
 FROM `students` 
-INNER JOIN `degrees`
+JOIN `degrees`
 ON `students`.`degree_id` = `degrees`.`id`
 WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 
@@ -32,7 +32,7 @@ WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 --  Neuroscienze
 SELECT `degrees`.`name`, `degrees`.`level`,`departments`.`name`
 FROM `degrees` 
-INNER JOIN `departments`
+JOIN `departments`
 ON `degrees`.`department_id` = `departments`.`id`
 WHERE `degrees`.`level` = "Magistrale"
 AND `departments`.`name` = "Dipartimento di Neuroscienze";
@@ -40,7 +40,7 @@ AND `departments`.`name` = "Dipartimento di Neuroscienze";
 --  3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 SELECT `courses`.`name`, `teachers`.`name`
 FROM `courses` 
-INNER JOIN `course_teacher`
+JOIN `course_teacher`
 ON `courses`.`id` = `course_teacher`.`course_id`
 INNER JOIN `teachers`
 ON `course_teacher`.`teacher_id` = `teachers`.`id`
@@ -49,9 +49,36 @@ WHERE `teachers`.`id` = 44
 --  4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui
 --  sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e
 --  nome
+SELECT `students`.`name`, `students`.`surname`, `courses`.*
+FROM `students`
+JOIN `courses`
+ON `students`.`degree_id` = `courses`.`id`
+ORDER BY `students`.`surname`, `students`.`name`
+
 --  5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT `degrees`.`name`, `courses`.`name`, `teachers`.`name`, `teachers`.`surname`
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`
+
 --  6. Selezionare tutti i docenti che insegnano nel Dipartimento di
 --  Matematica (54)
+SELECT DISTINCT `teachers`.`name`, `teachers`.`surname`, `departments`.`name`
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees`
+ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica'
+
 --  7. BONUS: Selezionare per ogni studente il numero di tentativi sostenuti
 --  per ogni esame, stampando anche il voto massimo. Successivamente,
 --  filtrare i tentativi con voto minimo 18.
